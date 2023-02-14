731b8d9e8c2e4da3d16458e5ac56ed7c537aa6cc
ellio <elliotpatra@gmail.com>
2023-02-14 22:31:19 +0200

    first initial

---
+++ src/main/java/com/unipi/msc/spaceroomapi/SpaceRoomApiApplication.java
@@ -0,0 +1,13 @@
+package com.unipi.msc.spaceroomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceRoomApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceRoomApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unipi/msc/spaceroomapi/SpaceRoomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipi.msc.spaceroomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceRoomApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
